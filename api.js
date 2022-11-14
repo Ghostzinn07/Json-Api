106e555873d5adc642ce4465502349576800092d
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2022-11-13 22:43:20 -0300

    ➰ | api.js
    📂 Json-Api/api.js
    ⬆️ 196B
    ❓ 7:43

--- api.js
+++ api.js
@@ -1,2 +1,7 @@
 const express = require("express");
 const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send(`📚 GitHub/Ghostzinn07`);
+    });
+app.listen(PORT, () => console.log("⬆️"));
