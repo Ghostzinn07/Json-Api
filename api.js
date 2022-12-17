4aee2d2751d2e667816129f2eefebc8076a00149
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2022-12-17 12:38:17 -0300

    📚 Update

--- api.js
+++ api.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const log = console.log;
 app.listen(3000, (res) => {
-console.log(`📚 GitHub/Ghostzinn07`)
+console.log(`> [🟢] Api Online`)
 });
 require('./src/rq.js')(app);
 app.get("/", async (req, res) => {
+  log('> [🛃] New Request')
   res.sendFile(path.join(__dirname, './src/index.html'));
 });
