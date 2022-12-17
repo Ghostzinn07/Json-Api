aaf2572fde4289607293818cb1661f4ad4fd7d35
Ghostzinn07 <99512155+Ghostzinn07@users.noreply.github.com>
2022-12-17 12:42:54 -0300

    📚 Update

--- src/rq.js
+++ src/rq.js
@@ -1,7 +1,10 @@
 module.exports = (app) => {
+const config = ('./config.js');
+const log = console.log;
 app.get('/api', async (req, res) => {
 var texto = req.query.text;
-if(!texto) return res.status(400).send("❓ Texto?");
+if(!texto) return res.status(400).send(`400 ERROR | [ https://${config.domain}/api[?text=(argumento)] ]`);
+log(`> [🔨] ${texto}`)
 res.send({
 "texto": texto,
 "github": "GitHub/Ghostzinn07"
